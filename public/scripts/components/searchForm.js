9c5a55a14453cd7789b38f51835e81d2f59b27b9
Jehyun <congmul@hotmail.com>
2021-10-12 13:39:56 -0700

    Search Style and functions

--- public/scripts/components/searchForm.js
+++ public/scripts/components/searchForm.js
@@ -1,15 +1,16 @@
 function searchFunction() {
     const searchInputForParentComEl = document.getElementById('search-form-input');
+    const searchList = document.getElementById('search-list');
     const searchbarOptionEl = document.getElementById('search-options');
-    
+    const liSearchOptionsEls = document.getElementsByClassName('li-search-options');
+    const searchFormForParentCompanyEl = document.getElementById('search-form-for-parentCompany');
+
+
     function onFucusSearchBar() {
         if(searchbarOptionEl.value === "productName"){
-            document.getElementById('search-list').style.opacity = "100%";
+            searchList.style.display = "block";
+            searchList.style.opacity = "100%";
         }
-    }
-    
-    function outFucusSearchBar() {
-        document.getElementById('search-list').style.opacity = "0%";
     }
     
     function onChangeSearchOption() {
@@ -19,8 +20,37 @@ function searchFunction() {
             searchInputForParentComEl.placeholder = 'Search for ' + "Products"
         }
     }
-    
+
+    function onClickListText(event) {
+        let seletecListforSearch = event.target.innerText;
+        searchInputForParentComEl.value = seletecListforSearch;
+        
+    }
+
+    function onClickSearchBarBtn(event) {
+        event.preventDefault();
+        console.log("Search Clicking", searchInputForParentComEl.value);
+    }
+
     searchInputForParentComEl.addEventListener("focus", onFucusSearchBar);
-    searchInputForParentComEl.addEventListener("focusout", outFucusSearchBar);
+    
     searchbarOptionEl.addEventListener('change', onChangeSearchOption)
+    for (const element of liSearchOptionsEls) {
+        element.addEventListener('click', onClickListText);
+    }
+    searchFormForParentCompanyEl.addEventListener('click', onClickSearchBarBtn);
+
+    window.onclick = function(event) {
+        let isSearchBar = searchInputForParentComEl === event.target;
+
+        // console.log("searchList.getAttribut", window.getComputedStyle(searchList).display)
+
+        if(window.getComputedStyle(searchList).display === "block"){
+            // console.log("BLOCK")
+            if(isSearchBar !== true){
+                searchList.style.display = "none";
+                searchList.style.opacity = "0%";
+            }
+        }
+      }
 }
